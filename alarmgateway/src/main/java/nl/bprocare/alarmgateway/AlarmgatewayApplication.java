bcd0d0ba929eb553c2411d7eca8cbd6a480204b1
hdjonge <herman.de.jonge@gmail.com>
2019-02-04 14:26:45 +0100

    commit project setup

---
+++ alarmgateway/src/main/java/nl/bprocare/alarmgateway/AlarmgatewayApplication.java
@@ -0,0 +1,14 @@
+package nl.bprocare.alarmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmgatewayApplication.class, args);
+	}
+
+}
+
