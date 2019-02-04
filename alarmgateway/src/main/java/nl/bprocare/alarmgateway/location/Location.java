100bd5f8e68d068792ed8d1a69461ac164b85289
hdjonge <herman.de.jonge@gmail.com>
2019-02-04 14:34:41 +0100

    Added class Location add static-file so that the folder static is not empty and can be commited

---
+++ alarmgateway/src/main/java/nl/bprocare/alarmgateway/location/Location.java
@@ -0,0 +1,64 @@
+package nl.bprocare.alarmgateway.location;
+
+public class Location {
+
+	private Long id;
+	private String street;
+	private String streetNumber;
+	private String town;
+	private String postalCode;
+	private String phoneNumber;
+	
+	public Location() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
