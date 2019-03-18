5b49cdb4b0af00708889c952582c7ea7bc0ee506
hdjonge <herman.de.jonge@gmail.com>
2019-03-18 15:39:48 +0100

    -minor changes

---
+++ alarmgateway/src/main/java/nl/bprocare/alarmgateway/dto/ViewLoctionDTO.java
@@ -0,0 +1,34 @@
+package nl.bprocare.alarmgateway.dto;
+
+public class ViewLoctionDTO {
+
+	private long id;
+	private String name;
+	private int status;
+	private String status_name;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getStatus_name() {
+		return status_name;
+	}
+	public void setStatus_name(String status_name) {
+		this.status_name = status_name;
+	}
+}
