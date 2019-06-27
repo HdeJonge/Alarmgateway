fb4499416b292e2456c5d5126b7ca4c824a52744
hdjonge <herman.de.jonge@gmail.com>
2019-06-27 12:16:45 +0200

    fixed project

--- alarmgateway/src/main/java/nl/bprocare/alarmgateway/controllers/AlarmgatewayRestController.java
+++ alarmgateway/src/main/java/nl/bprocare/alarmgateway/controllers/AlarmgatewayRestController.java
@@ -5,16 +5,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import nl.bprocare.alarmgateway.pojo.Alarmgateway;
 import nl.bprocare.alarmgateway.repository.AlarmgatewayDataTableRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class AlarmgatewayRestController {
 	@Autowired
@@ -22,9 +19,7 @@ public class AlarmgatewayRestController {
 	
 	@GetMapping("/restgateways")
 	public DataTablesOutput<Alarmgateway> getAlarmgateways(@Valid DataTablesInput input){
-		System.out.println(input);
-		DataTablesOutput<Alarmgateway> findAll = repository.findAll(input);
-		return findAll;
+		return repository.findAll(input);
 	}
 
 }
